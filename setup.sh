fa645cc974b122a6c7cc461b374c57097bfa9935
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-10-06 23:05:30 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,133 +1,11 @@
 #!/usr/bin/env bash
-################################################################################
-# This is property of eXtremeSHOK.com
-# You are free to use, modify and distribute, however you may not remove this notice.
-# Copyright (c) Adrian Jon Kriel :: admin@extremeshok.com
-################################################################################
-#
-# Script updates can be found at: https://github.com/extremeshok/xshok-docker
-#
-# Ubuntu to optimised docker host, will optimise and install utilities, docker, docker-composer
-#
-# License: BSD (Berkeley Software Distribution)
-#
-################################################################################
-#
-# Assumptions: Ubuntu installed
-#
-# Tested on KVM, VirtualBox and Dedicated Server
-#
-# Notes:
-# to disable the MOTD banner, set the env NO_MOTD_BANNER to true (export NO_MOTD_BANNER=true)
-# to set the swapfile size to 1GB, set the env SWAPFILE_SIZE to 1 (export SWAPFILE_SIZE=1)
-#
-# Usage:
-# wget https://raw.githubusercontent.com/extremeshok/xshok-docker/master/xshok-ubuntu-docker-host.sh -O xshok-ubuntu-docker-host.sh && chmod +x xshok-ubuntu-docker-host.sh && ./xshok-ubuntu-docker-host.sh
-#
-################################################################################
-#
-#    THERE ARE NO USER CONFIGURABLE OPTIONS IN THIS SCRIPT
-#
-################################################################################
 
 # Set the local
 export LANG="en_US.UTF-8"
 export LC_ALL="C"
 
-if [ "$(lsb_release -i 2>/dev/null | cut -f 2 | xargs)" != "Ubuntu" ] ; then
-  echo "ERROR: This script only supports Ubuntu"
-  exit 1
-fi
-
-if [ -f "/etc/extremeshok" ] ; then
-  echo "Script can only be run once"
-  exit 1
-fi
-
 ## Force APT to use IPv4
 echo -e "Acquire::ForceIPv4 \"true\";\\n" > /etc/apt/apt.conf.d/99force-ipv4
-
-## Refresh the package lists
-apt-get update > /dev/null 2>&1
-
-## Remove conflicting utilities
-/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' purge snapd ntp openntpd snap lxd bind bind9 bluez docker docker-engine docker.io containerd runc
-
-## Update
-/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' dist-upgrade
-
-## Install common system utilities
-/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install \
-apt-transport-https \
-aptitude \
-axel \
-build-essential \
-ca-certificates \
-curl \
-dialog \
-dos2unix \
-dpkg-dev \
-git \
-gnupg-agent \
-htop \
-iftop \
-iotop \
-iperf \
-ipset \
-iptraf \
-logrotate \
-mlocate \
-nano \
-net-tools \
-pigz \
-python3-pip \
-software-properties-common \
-sshpass \
-tmux \
-unzip zip \
-vim \
-vim-nox \
-wget \
-whois
-
-## Remove no longer required packages and purge old cached updates
-/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' autoremove
-/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' autoclean
-
-## Detect if virtual machine and install guest agent
-if [ "$(dmidecode -s system-manufacturer | xargs)" == "QEMU" ] || [ "$(systemd-detect-virt | xargs)" == "kvm" ] ; then
-  echo "QEMU Detected, installing guest agent"
-  /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install qemu-guest-agent
-elif [ "$(systemd-detect-virt | xargs)" == "vmware" ] ; then
-  echo "VMware Detected, installing vm-tools"
-  /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install open-vm-tools
-elif [ "$(systemd-detect-virt | xargs)" == "oracle" ] ; then
-  echo "Virtualbox Detected, installing guest-utils"
-  /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install virtualbox-guest-utils
-fi
-
-## Detect cloud-init device and install cloud-init
-if [ "$(systemd-detect-virt | xargs)" != "None" ] ; then
-  if [ -r "/dev/sr0" ] ; then #sr0 = link for cdrom
-    if [ "$(blkid -o export /dev/sr0 | grep "LABEL" | cut -d'=' -f 2 | xargs)" == "cidata" ] ; then
-      echo "Cloud-init device Detected, installing cloud-init"
-      /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install cloud-init
-    fi
-  fi
-fi
-
-## Disable portmapper / rpcbind (security)
-systemctl disable rpcbind
-systemctl stop rpcbind
-
-## Disable local dns server, do not disable systemd-resolved as this breaks nameservers configured with netplan
-sed -i 's|#DNSStubListener=yes|DNSStubListener=no|g' /etc/systemd/resolved.conf
-sed -i 's|DNSStubListener=yes|DNSStubListener=no|g' /etc/systemd/resolved.conf
-sed -i 's|#DNSStubListener=no|DNSStubListener=no|g' /etc/systemd/resolved.conf
-rm -rf /etc/resolv.conf
-ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
-systemctl enable systemd-resolved.service
-
 
 ## Set Timezone to UTC and enable NTP
 timedatectl set-timezone Asia/Ho_Chi_Minh
@@ -154,7 +32,7 @@ sysctl -p /etc/sysctl.conf
 
 ## Increase max FD limit / ulimit
 cat <<'EOF' >> /etc/security/limits.conf
-# eXtremeSHOK.com Increase max FD limit / ulimit
+# Increase max FD limit / ulimit
 * soft     nproc          256000
 * hard     nproc          256000
 * soft     nofile         256000
@@ -175,21 +53,8 @@ echo 'session required pam_limits.so' | tee -a /etc/pam.d/runuser-l
 ## Set ulimit for the shell user
 cd ~ && echo "ulimit -n 256000" >> .bashrc ; echo "ulimit -n 256000" >> .profile
 
-## Enable unattended upgrades
-cat <<'EOF' > /etc/apt/apt.conf.d/20auto-upgrades
-APT::Periodic::Update-Package-Lists "1";
-APT::Periodic::Unattended-Upgrade "1";
-APT::Periodic::AutocleanInterval "7";
-EOF
-# Enable Update Origins
-sed -i 's|\/\/.*"\${distro_id}:\${distro_codename}"|"\${distro_id}:\${distro_codename}"|' /etc/apt/apt.conf.d/50unattended-upgrades
-sed -i 's|\/\/.*"\${distro_id}:\${distro_codename}-security"|"\${distro_id}:\${distro_codename}-security"|' /etc/apt/apt.conf.d/50unattended-upgrades
-sed -i 's|\/\/.*"\${distro_id}:\${distro_codename}-updates"|"\${distro_id}:\${distro_codename}-updates"|' /etc/apt/apt.conf.d/50unattended-upgrades
-
-
 ## Increase kernel max Key limit
 cat <<EOF > /etc/sysctl.d/99-xs-maxkeys.conf
-# eXtremeSHOK.com
 # Increase kernel max Key limit
 kernel.keys.root_maxkeys=1000000
 kernel.keys.maxkeys=1000000
@@ -197,7 +62,6 @@ EOF
 
 ## Enable TCP BBR congestion control
 cat <<EOF > /etc/sysctl.d/99-xs-kernel-bbr.conf
-# eXtremeSHOK.com
 # TCP BBR congestion control
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
@@ -205,7 +69,6 @@ EOF
 
 ## Memory Optimising
 cat <<EOF > /etc/sysctl.d/99-xs-memory.conf
-# eXtremeSHOK.com
 # Memory Optimising
 vm.min_free_kbytes=65536
 vm.nr_hugepages=72
@@ -213,24 +76,14 @@ vm.nr_hugepages=72
 vm.overcommit_memory = 1
 EOF
 
-## Enable IPv6
-cat <<EOF > /etc/sysctl.d/99-xs-ipv6.conf
-# eXtremeSHOK.com
-# Enable IPv6
-net.ipv6.conf.all.disable_ipv6 = 0
-net.ipv6.conf.default.disable_ipv6 = 0
-EOF
-
 ## TCP fastopen
 cat <<EOF > /etc/sysctl.d/99-xs-tcp-fastopen.conf
-# eXtremeSHOK.com
 # TCP fastopen
 net.ipv4.tcp_fastopen=3
 EOF
 
 ## FS Optimising
 cat <<EOF > /etc/sysctl.d/99-xs-tcp-fastopen.conf
-# eXtremeSHOK.com
 # FS Optimising
 fs.nr_open=12000000
 fs.file-max=9000000
@@ -238,7 +91,6 @@ EOF
 
 ## Net optimising
 cat <<EOF > /etc/sysctl.d/99-xs-net.conf
-# eXtremeSHOK.com
 net.core.netdev_max_backlog=8192
 net.core.optmem_max=8192
 net.core.rmem_max=16777216
